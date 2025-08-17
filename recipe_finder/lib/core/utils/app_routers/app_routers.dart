e346bd127080ab176bbcfacdf4e0e9f027b282e2
Kyria-Ashraf <keirayaashraf@gmail.com>
2025-08-17 20:19:49 +0300

    ad home view to app routers

--- recipe_finder/lib/core/utils/app_routers/app_routers.dart
+++ recipe_finder/lib/core/utils/app_routers/app_routers.dart
@@ -1,4 +1,5 @@
 import 'package:go_router/go_router.dart';
+import 'package:recipe_finder/features/home_feature/presentation/views/home_view.dart';
 import 'package:recipe_finder/features/login_feature/presentation/views/login_view.dart';
 import 'package:recipe_finder/features/onboarding_feature/presentation/views/onboaring_view.dart';
 import 'package:recipe_finder/features/signup_feature/presentation/views/signup_view.dart';
@@ -7,6 +8,7 @@ abstract class AppRouters {
   static const String konboardingView = '/';
   static const String kloginView = '/loginView';
   static const String ksignupView = '/signupView';
+  static const String khomeView = '/homeView';
 
   static final routers = GoRouter(routes: [
     GoRoute(
@@ -20,6 +22,10 @@ abstract class AppRouters {
     GoRoute(
       path: ksignupView,
       builder: (context, state) => const SignupView(),
+    ),
+    GoRoute(
+      path: khomeView,
+      builder: (context, state) => const HomeView(),
     ),
   ]);
 }
