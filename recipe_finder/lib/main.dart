8352d3b8596dc075b4aa0a3cfaa2b3914311eb35
Kyria-Ashraf <keirayaashraf@gmail.com>
2025-09-03 13:05:14 +0300

    added DevicePreview for different screen sizes

--- recipe_finder/lib/main.dart
+++ recipe_finder/lib/main.dart
@@ -1,8 +1,15 @@
+import 'package:device_preview/device_preview.dart';
 import 'package:flutter/material.dart';
 import 'package:recipe_finder/core/utils/app_routers/app_routers.dart';
 
 void main() {
-  runApp(const MyApp());
+  runApp(DevicePreview(
+    enabled: true,
+    tools: const [
+      ...DevicePreview.defaultTools,
+    ],
+    builder: (context) => const MyApp(),
+  ));
 }
 
 class MyApp extends StatelessWidget {
